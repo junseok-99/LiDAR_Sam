8ecb158fde0470b040bb78a28f2b2cb048772c77
junseoks-99 <angec5645@naver.com>
2023-03-31 21:00:51 +0900

    Initial Commit

---
+++ LiDAR_Sam/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  LiDAR_Sam
+//
+//  Created by 장준석 on 2023/03/31.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
